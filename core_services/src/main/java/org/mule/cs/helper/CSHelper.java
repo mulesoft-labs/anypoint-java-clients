f25ce3047981f3e2d173a6b12de97b0dd601db50
machaval <mariano.achaval@mulesoft.com>
2019-05-10 18:09:11 -0300

    Add new version of core sevices

---
+++ core_services/src/main/java/org/mule/cs/helper/CSHelper.java
@@ -0,0 +1,11 @@
+package org.mule.cs.helper;
+
+import org.mule.cs.api.CoreServicesAPIReferenceClient;
+
+public class CSHelper {
+
+    public String login(String userName, String password) {
+        CoreServicesAPIReferenceClient client = CoreServicesAPIReferenceClient.create();
+        return null;
+    }
+}
